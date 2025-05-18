4b0ff26a7cfb871da6d507a947b73ea87ac6855c
evanovar <evanyzited@gmail.com>
2025-05-18 20:06:06 +0700

    Removed useless lines of code

--- RobloxMultiInstance/Program.cs
+++ RobloxMultiInstance/Program.cs
@@ -12,13 +12,15 @@ namespace RobloxMultiInstance773Fix
 
         static void Main(string[] args)
         {
-            Console.Title = "Roblox Multi Instance Enabler";
-            Console.WriteLine("\r\n  __  __       _ _   _   _____           _                       \r\n |  \\/  |     | | | (_) |_   _|         | |                      \r\n | \\  / |_   _| | |_ _    | |  _ __  ___| |_ __ _ _ __   ___ ___ \r\n | |\\/| | | | | | __| |   | | | '_ \\/ __| __/ _` | '_ \\ / __/ _ \\\r\n | |  | | |_| | | |_| |  _| |_| | | \\__ \\ || (_| | | | | (_|  __/\r\n |_|  |_|\\__,_|_|\\__|_| |_____|_| |_|___/\\__\\__,_|_| |_|\\___\\___|\r\n                                                                 \r\n                                                                 \r\n");
-            Console.WriteLine("Multi Instance with TeleportService support");
-            Console.WriteLine("Made by evanovar :)\n");
-
             try
             {
+                Console.Title = "Roblox Multi Instance Enabler + 773 Fix";
+
+                Console.WriteLine("\r\n  __  __       _ _   _   _____           _                       \r\n |  \\/  |     | | | (_) |_   _|         | |                      \r\n | \\  / |_   _| | |_ _    | |  _ __  ___| |_ __ _ _ __   ___ ___ \r\n | |\\/| | | | | | __| |   | | | '_ \\/ __| __/ _` | '_ \\ / __/ _ \\\r\n | |  | | |_| | | |_| |  _| |_| | | \\__ \\ || (_| | | | | (_|  __/\r\n |_|  |_|\\__,_|_|\\__|_| |_____|_| |_|___/\\__\\__,_|_| |_|\\___\\___|\r\n                                                                 \r\n                                                                 \r\n");
+                Console.WriteLine("Multi Instance + 773 error code fix");
+                Console.WriteLine("Made by evanovar :)\n");
+
+                // Try to create mutex to allow multi-instance
                 robloxMutex = new Mutex(true, "ROBLOX_singletonEvent", out bool createdNew);
 
                 if (createdNew)
@@ -28,47 +30,43 @@ namespace RobloxMultiInstance773Fix
                 }
                 else
                 {
-                    Console.ForegroundColor = ConsoleColor.Red;
-                    Console.WriteLine("[ERROR] Mutex already exists. Another instance might be running.");
+                    Console.ForegroundColor = ConsoleColor.Yellow;
+                    Console.WriteLine("[WARNING] Mutex already exists. Another instance might be running.");
                 }
                 Console.ResetColor();
 
+                // Attempt to lock cookies file
                 string cookiesPath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), @"Roblox\LocalStorage\RobloxCookies.dat");
-                string noFixFile = Path.Combine(Environment.CurrentDirectory, "no773fix.txt");
 
-                bool apply773Fix = File.Exists(cookiesPath) && !File.Exists(noFixFile);
-
-                if (!apply773Fix)
-                {
-                    Console.ForegroundColor = ConsoleColor.Red;
-                    Console.WriteLine($"[ERROR] Not applying 773 fix: Cookies file exists = {File.Exists(cookiesPath)}, 'no773fix.txt' exists = {File.Exists(noFixFile)}");
-                    Console.ResetColor();
-                }
-                else
+                if (File.Exists(cookiesPath))
                 {
                     try
                     {
                         cookiesLockStream = new FileStream(cookiesPath, FileMode.Open, FileAccess.ReadWrite, FileShare.None);
                         Console.ForegroundColor = ConsoleColor.Green;
-                        Console.WriteLine("[INFO] Error 773 has been fixed successfully.");
-                        Console.ResetColor();
+                        Console.WriteLine("[INFO] Error 773 fix applied. Locked RobloxCookies.dat.");
                     }
                     catch (IOException)
                     {
                         Console.ForegroundColor = ConsoleColor.Red;
-                        Console.WriteLine("[ERROR] Could not lock RobloxCookies.dat (file might be in use by another instance).");
-                        Console.ResetColor();
+                        Console.WriteLine("[ERROR] Could not lock RobloxCookies.dat. It may already be locked.");
                     }
                 }
+                else
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("[ERROR] Cookies file not found. 773 fix skipped.");
+                }
 
+                Console.ResetColor();
                 Console.WriteLine("\nKeep this window open while launching Roblox instances.");
-                Console.WriteLine("Press any key to exit and disable multi instance...");
+                Console.WriteLine("Press any key to exit...");
                 Console.ReadKey();
             }
             catch (Exception ex)
             {
                 Console.ForegroundColor = ConsoleColor.Red;
-                Console.WriteLine("[ERROR] Unexpected error: " + ex.Message);
+                Console.WriteLine("❌ Unexpected error: " + ex.Message);
                 Console.ResetColor();
             }
             finally
